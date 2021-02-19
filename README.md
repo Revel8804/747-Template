d7e7f05bdd6a0459ddf74a9431077add7aafa99d
duke8804 <kenny@revelcomtech.com>
2021-02-19 14:51:15 -0600

    Working mostly

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # 747-Template
+********************************************************************************************************************
+ALERT
+This will install Chocolatey, Image Magick, and Blender if it is not installed already.
+You can learn more about Chocolatey here https://chocolatey.org/
+You can learn more about Image Magick here https://chocolatey.org/packages/imagemagick
+You can learn more about Blender here https://chocolatey.org/packages/blender
+********************************************************************************************************************
+
+Great News I have received permission from HughesMDflyer4 to include the MSFSLayoutGenerator.
+You can learn all about it here https://github.com/HughesMDflyer4/MSFSLayoutGenerator
+
+Only for Windows 64bit systems. If there is a large demand for 32bit for some reason, I will update.
+The executable will open the file for you in blender and wait until you close blender to do the rest.
+This will allow you to modify the interior and exterior.
+
+How to use.
+Run the .exe It should open Blender. Make changes as you wish. Save over textures.
+Once you close blender then the program will finish converting and moving files for you.
+
+TO DO:
+Clean up the blender file. Outside the fuselage parts are goofy.
+Satisfy curious of what it would take to add seats to the back.
+
+Thank you to HughesMDflyer4 for use of https://github.com/HughesMDflyer4/MSFSLayoutGenerator
---
+++ XboxVersionMSFS747Livery.ps1
@@ -0,0 +1,110 @@
+function New-Livery {
+    if (Test-Path $livloc) {$goon = Read-Host -Prompt "Folder already exists, Overwrite? Y/N"}
+    if ($goon -eq "Y" -or $goon -eq "y" -or -not(Test-Path $livloc)) {
+        Remove-Item -Path $livloc -Force -Recurse
+        New-Item -Path $livloc\SimObjects\Airplanes\Asobo_B747_8i_$airfold\MODEL.$airfold -ItemType Directory
+        New-Item -Path $livloc\SimObjects\Airplanes\Asobo_B747_8i_$airfold\MODEL.AI_$airfold -ItemType Directory
+        New-Item -Path $livloc\SimObjects\Airplanes\Asobo_B747_8i_$airfold\TEXTURE.$airfold -ItemType Directory
+        
+    }
+}
+function Move-Files {
+    $moveddsfile = Get-ChildItem -Path $PSScriptRoot\texture -Filter *.dds
+    foreach ($item in $moveddsfile) {
+        Copy-Item $PSScriptRoot\texture\json\$item.json -destination "$livloc\SimObjects\Airplanes\Asobo_B747_8i_$airfold\TEXTURE.$airfold"
+    }
+    Copy-Item $PSScriptRoot\Files\manifest.json $livloc\manifest.json
+    Copy-Item $PSScriptRoot\Files\layout.json $livloc\layout.json
+    Copy-Item $PSScriptRoot\Files\aircraft.cfg $livloc\SimObjects\Airplanes\Asobo_B747_8i_$airfold\aircraft.cfg
+    Copy-Item $PSScriptRoot\Files\model.cfg $livloc\SimObjects\Airplanes\Asobo_B747_8i_$airfold\MODEL.$airfold\model.cfg
+    Copy-Item $PSScriptRoot\Files\model.cfg $livloc\SimObjects\Airplanes\Asobo_B747_8i_$airfold\MODEL.AI_$airfold\model.cfg
+    Copy-Item $PSScriptRoot\Files\texture.cfg $livloc\SimObjects\Airplanes\Asobo_B747_8i_$airfold\TEXTURE.$airfold\texture.cfg
+    Get-ChildItem -path $PSScriptRoot\texture -filter *.dds | Copy-Item -destination "$livloc\SimObjects\Airplanes\Asobo_B747_8i_$airfold\TEXTURE.$airfold"
+}
+
+function Update-ManifestJson {
+    $file = Get-content -path $livloc\manifest.json
+    $newfile = $file -replace 'AIRLINE', ("$airline")
+    $newfile | Set-content -path $livloc\manifest.json
+    $file = Get-content -path $livloc\manifest.json
+    $newfile = $file -replace 'PERSONA', ("$name")
+    $newfile | Set-content -path $livloc\manifest.json
+}
+
+function Update-AircraftConfig {
+    # Im sure there is a better way, but this worked.
+    $file = Get-content -path "$livloc\SimObjects\Airplanes\Asobo_B747_8i_$airfold\aircraft.cfg"
+    $newfile = $file -replace 'AIRFOLD" ', ("$airfold" + '" ')
+    $newfile | Set-content -path "$livloc\SimObjects\Airplanes\Asobo_B747_8i_$airfold\aircraft.cfg"
+    $file = Get-content -path "$livloc\SimObjects\Airplanes\Asobo_B747_8i_$airfold\aircraft.cfg"
+    $newfile = $file -replace 'AIRLINE" ', ("$airline" + '" ')
+    $newfile | Set-content -path "$livloc\SimObjects\Airplanes\Asobo_B747_8i_$airfold\aircraft.cfg"
+    $file = Get-content -path "$livloc\SimObjects\Airplanes\Asobo_B747_8i_$airfold\aircraft.cfg"
+    $newfile = $file -replace 'PERSONA" ', ("$name" + '" ')
+    $newfile | Set-content -path "$livloc\SimObjects\Airplanes\Asobo_B747_8i_$airfold\aircraft.cfg"
+    $file = Get-content -path "$livloc\SimObjects\Airplanes\Asobo_B747_8i_$airfold\aircraft.cfg"
+    $newfile = $file -replace 'ATCID" ', ("$atcid" + '" ')
+    $newfile | Set-content -path "$livloc\SimObjects\Airplanes\Asobo_B747_8i_$airfold\aircraft.cfg"
+    $file = Get-content -path "$livloc\SimObjects\Airplanes\Asobo_B747_8i_$airfold\aircraft.cfg"
+    $newfile = $file -replace 'ICAO" ', ("$icao" + '" ')
+    $newfile | Set-content -path "$livloc\SimObjects\Airplanes\Asobo_B747_8i_$airfold\aircraft.cfg"
+    $file = Get-content -path "$livloc\SimObjects\Airplanes\Asobo_B747_8i_$airfold\aircraft.cfg"
+    $newfile = $file -replace '##" ', ("$flightnumber" + '" ')
+    $newfile | Set-content -path "$livloc\SimObjects\Airplanes\Asobo_B747_8i_$airfold\aircraft.cfg"
+}
+
+function Convert-ToDDS {
+    $convertfile = Get-ChildItem -Path $PSScriptRoot\texture -Include *.png -Recurse | Where-Object {$_.LastWriteTime -lt (get-date).AddMinutes(-2)}
+    magick.exe mogrify -format DDS $convertfile
+}
+function Update-LayoutJson {
+    Start-Process -FilePath $PSScriptRoot\Files\MSFSLayoutGenerator.exe -ArgumentList $livloc\layout.json
+}
+
+function Install-NeededPrograms {
+    $imagemagick = $null -ne (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object { $_.DisplayName -like "*ImageMagick*" })
+    $blender = $null -ne (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object { $_.DisplayName -like "*Blender*" })
+    if (-not (Test-Path "C:\ProgramData\chocolatey\choco.exe")) {
+        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    }
+    if (-not $imagemagick) {
+        Choco install ImageMagick -y
+    }
+    if (-not $blender) {
+        Choco install Blender -y
+    }
+}
+
+function Open-Blender {
+    $blenderloc = Get-ChildItem -Path "C:\Program Files\Blender Foundation\" -Include blender.exe -File -Recurse -ErrorAction SilentlyContinue
+    $blenderloc.FullName
+    Start-Process -Wait -FilePath $blenderloc -ArgumentList $PSScriptRoot\747.blend
+}
+
+function Expand-BlenderFile {
+    Expand-Archive $PSScriptRoot\747.zip -DestinationPath $PSScriptRoot
+}
+
+# Form boxes working on making it one box.
+[void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
+$name = [Microsoft.VisualBasic.Interaction]::InputBox("Enter your persona name", "Persona", "duke8804")
+$airline = [Microsoft.VisualBasic.Interaction]::InputBox("Enter your airline or livery name", "Airline", "Bankrupt Airlines")
+$atcid = [Microsoft.VisualBasic.Interaction]::InputBox("Enter your airline ATC ID (Tail Number)", "ATC ID", "BKRPT88")
+$icao = [Microsoft.VisualBasic.Interaction]::InputBox("Enter your airline ICAO name", "ICAO", "BKRPT")
+$flightnumber = [Microsoft.VisualBasic.Interaction]::InputBox("Enter your airline flight number", "Flight Number", "88")
+$airfold = $airline.replace(' ','')
+$app = Get-AppxPackage -Name Microsoft.FlightSimulator
+$appname = $app.PackageFamilyName
+$apploc = "$env:LOCALAPPDATA\Packages\$appname\LocalCache\Packages\Community"
+$livloc = "$apploc\B747-8i-$airfold"
+
+# Run functions
+Install-NeededPrograms
+Expand-BlenderFile
+Open-Blender
+New-Livery -livloc $livloc -airfold $airfold
+Convert-ToDDS
+Move-Files
+Update-ManifestJson
+Update-AircraftConfig
+Update-LayoutJson
